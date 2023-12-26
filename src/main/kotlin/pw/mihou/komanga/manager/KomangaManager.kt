f1dd3e88be7a8f3df8925ab1090d6caf75f959f8
Shindou Mihou <git@mihou.pw>
2023-12-26 11:18:36 +0800

    feat: add cli runner

---
+++ src/main/kotlin/pw/mihou/komanga/manager/KomangaManager.kt
@@ -0,0 +1,53 @@
+package pw.mihou.komanga.manager
+
+import pw.mihou.komanga.Komanga
+import pw.mihou.komanga.models.MigrationKind
+
+typealias KomangaTask = suspend () -> Unit
+
+/**
+ * [KomangaManager] is a simple manager that enables developers to quickly integrate
+ * Komanga migrations onto their program commandline arguments.
+ */
+object KomangaManager {
+    /**
+     * [run] selectively looks into the [args] of the program to identify specific
+     * database migration-related tasks to execute and runs them all.
+     *
+     * @return true when there was a task executed.
+     */
+    suspend fun run(args: List<String>): Boolean {
+        val tasks = mutableListOf<KomangaTask>()
+        for (raw in args) {
+            val arg = raw.lowercase()
+            when(arg) {
+                "--migrate-index" -> {
+                    tasks += { Komanga.migrate(MigrationKind.INDEX) }
+                }
+                "--migrate-data" -> {
+                    tasks += { Komanga.migrate(MigrationKind.DATA) }
+                }
+                "--migrate-collection" -> {
+                    tasks += { Komanga.migrate(MigrationKind.COLLECTION) }
+                }
+                "--rollback-index" -> {
+                    tasks += { Komanga.rollback(MigrationKind.INDEX) }
+                }
+                "--rollback-data" -> {
+                    tasks += { Komanga.migrate(MigrationKind.DATA) }
+                }
+                "--rollback-collection" -> {
+                    tasks += { Komanga.migrate(MigrationKind.COLLECTION) }
+                }
+            }
+        }
+        if (tasks.isEmpty()) {
+            return false
+        }
+
+        for (task in tasks) {
+            task()
+        }
+        return true
+    }
+}
